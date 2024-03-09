148a20a2dfe10607546c96ab5f43294cfe00ed19
Romsgs <rsalobrena@gmail.com>
2024-03-09 02:29:15 -0300

    :hammer: error handling and typo fix

--- src/createWallet.ts
+++ src/createWallet.ts
@@ -1,6 +1,12 @@
-
-
-export default function createWallet(test:boolean =true):object {
+interface WalletObject {
+  // wallet address hash
+  walletAddress: String;
+  // mnemonic words
+  seed: String;
+  // wallet hash
+  generatedWallet: String;
+}
+export default function createWallet(test: boolean = true): WalletObject {
   const bip32 = require("bip32");
   const bip39 = require("bip39");
   const bitcoin = require("bitcoinjs-lib");
@@ -8,34 +14,40 @@ export default function createWallet(test:boolean =true):object {
   // definir a rede
   // bitcoin - rede principal - mainnet
   // testnet - rede de teste - testnet
-  const network = test? bitcoin.networks.testnet : bitcoin.networks.bitcoin;
+  const network = test ? bitcoin.networks.testnet : bitcoin.networks.bitcoin;
 
   // derivação de carteiras no HD
-  const path = `m/49'/${test?1:0}'/0'/0`;
+  const path = `m/49'/${test ? 1 : 0}'/0'/0`;
 
   // criando as palavras do mnemonic para a seed
-  let mnemonic = bip39.generateMnemonic();
+  const mnemonic = bip39.generateMnemonic();
+  if (!mnemonic) {
+    throw new Error("Error generating mnemonic");
+  }
   const seed = bip39.mnemonicToSeedSync(mnemonic);
 
   // criando a raiz da carteira
-  let root = bip32.fromSeed(seed, network);
+  const root = bip32.fromSeed(seed, network);
 
   // criando uma conta - par pvt-pub keys
-  let account = root.derivePath(path);
+  const account = root.derivePath(path);
   // atribuindo um node
-  let node = account.derive(0).derive(0);
+  const node = account.derive(0).derive(0);
 
-  let btcAddress = bitcoin.payments.p2pkh({
+  const btcAddress = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
     network: network,
   }).address;
+  if (!btcAddress) {
+    throw new Error("Failed to generate a valid Bitcoin address");
+  }
   // console.log("Carteira gerada ");
   // console.log("Endereço  ", btcAddress);
   // console.log("Carteira gerada: ", node.toWIF());
   // console.log("seed ", mnemonic);
   return {
-    walletAdress: btcAddress,
+    walletAddress: btcAddress,
     seed: mnemonic,
-    generatedWallet: node.toWIF()
-  }
+    generatedWallet: node.toWIF(),
+  };
 }
